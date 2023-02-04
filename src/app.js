b35f5603ec439722a14f4d91c8c8d23324a4e924
pepeboricua <joseoronozjr@gmail.com>
2023-02-04 00:45:26 +0000

    Complete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "their"];
+  let adjs = ["great", "big", "small"];
+  let nouns = ["jogger", "racoon", "pilot"];
+  let tlds = [".com", ".net", ".gov", ".us", ".app"];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${pronoun}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
